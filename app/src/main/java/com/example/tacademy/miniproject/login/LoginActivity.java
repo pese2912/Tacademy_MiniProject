32af1e0b49ee518cffeef94340b3c485131e8292
pese2912 <pese2912@naver.com>
2016-05-09 11:35:25 +0900

    activity

---
+++ app/src/main/java/com/example/tacademy/miniproject/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.tacademy.miniproject.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tacademy.miniproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
